3f5fdb013216adebdb9d72cefb3a90b873316f74
thdiep <tranhoangdiepbp@gmail.com>
2026-01-20 14:25:47 +0700

    chang port 5001 sang 5000

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 const AuthContext = createContext(null);
 
 // API base URL
-const API_URL = 'http://127.0.0.1:5001/api';
+const API_URL = 'http://127.0.0.1:5000/api';
 
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
